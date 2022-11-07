e602f58f82d1aceb948b8363c666f1705d05448d
dimanchickos <dimanchickos@gmail.com>
2022-11-07 19:46:35 +0300

    make_order_first_try

--- tgbot/handlers/order.py
+++ tgbot/handlers/order.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineQuery, InlineQueryResultArticle, \
+    InputTextMessageContent
+from aiogram.utils.markdown import hide_link
 
 from tgbot.keyboards.order_kb import meal_types_kb, menu_by_days_kb, meal_type_cb
 from tgbot.misc.states import Order
@@ -14,7 +16,7 @@ async def order_start(message: Message, state: FSMContext):
 async def display_menu(cb: CallbackQuery, state: FSMContext, callback_data: dict):
     await state.update_data(meal_type=callback_data['type'])
     await Order.Menu.set()
-    for i in range(1,4):
+    for i in range(1, 4):
         menu_by_days_kb.insert(
             InlineKeyboardButton(
                 text=f"День {i}",
@@ -22,12 +24,33 @@ async def display_menu(cb: CallbackQuery, state: FSMContext, callback_data: dict
             )
         )
     await cb.message.edit_text(f"Вы выбрали рацион <b>{callback_data['type']}</b>"
-                               f" на <b>{callback_data['calories']} ккал.</b>\n"
+                               f" на <b>{callback_data['calories']} ккал/день.</b>\n"
                                f"С помощью клавиш ниже можно ознакомиться с меню на ближайшие дни.",
                                reply_markup=menu_by_days_kb)
 
 
+async def days_menu(query: InlineQuery):
+    await query.answer(
+        results=[
+            InlineQueryResultArticle(
+                id="1",
+                title="Белковый омлет с салатом",
+                input_message_content=InputTextMessageContent(
+                    message_text=f"{hide_link('http://cc42190.tmweb.ru/wp-content/uploads/2022/10/П1-1-768x963.png')}"
+                                 f"*Состав*:\n"
+                                 f"Бла *Бла*\n"
+                                 f"Бла",
+                    parse_mode="HTML"
+                ),
+                url="https://core.telegram.org/bots/api#inline-mode",
+                thumb_url="http://cc42190.tmweb.ru/wp-content/uploads/2022/10/П1-1-768x963.png",
+                description="БЖУ"
+            )
+        ]
+    )
+
+
 def register_order(dp: Dispatcher):
     dp.register_message_handler(order_start, lambda message: message.text == "🏁 Сделать заказ", state="*")
-    dp.register_callback_query_handler(display_menu, meal_type_cb.filter(),state=Order.Meal_type)
-    dp.register_inline_handler()
+    dp.register_callback_query_handler(display_menu, meal_type_cb.filter(), state=Order.Meal_type)
+    dp.register_inline_handler(days_menu, text="")
